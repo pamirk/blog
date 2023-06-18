b1218dddac1921bd5dfef86721a2269b00b41a49
Pamir khan <pmysteryk@gmail.com>
2023-06-18 14:56:52 +0500

    feat: added for loop logic to index.html template

--- app/routes.py
+++ app/routes.py
@@ -6,4 +6,14 @@ from app import app
 @app.route('/index')
 def index():
     user = {'username': 'Pamirkhan'}
-    return render_template('index.html', title='Home', user=user)
+    posts = [
+        {
+            'author': {'username': 'Pamir'},
+            'body': 'Beautiful day in Quetta!'
+        },
+        {
+            'author': {'username': 'khan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
