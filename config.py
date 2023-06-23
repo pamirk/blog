1441f8b0eafe3dab6b2376f8e3f9faf6b45d6440
Pamir khan <pmysteryk@gmail.com>
2023-06-23 19:30:28 +0500

    feat: added SQLALCHEMY confi guration

--- config.py
+++ config.py
@@ -1,5 +1,9 @@
 import os
 
 
+basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'MY-VERY-PRIVATE-AND-SECRETE-KEY-THAT-NO-ONE-CAN-GUESS'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+                              'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS=False
