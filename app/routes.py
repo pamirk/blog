9ee3eb1fa7f552332976b1f9134c51aeead74bdb
Pamir khan <pmysteryk@gmail.com>
2023-07-07 11:15:59 +0500

    feat: added logging user out feature

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from flask import render_template, flash, redirect, url_for, request
-from flask_login import current_user, login_user
+from flask_login import current_user, login_user, logout_user
 from werkzeug.urls import url_parse
 
 from app import app
@@ -42,3 +42,9 @@ def login():
             next_page = url_for('index')
         return redirect(next_page)
     return render_template('login.html', title='Sign In', form=form)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
