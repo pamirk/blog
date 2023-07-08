430c87b080fc18770f386b4fa9705f88f187b566
Pamir khan <pmysteryk@gmail.com>
2023-07-08 13:04:55 +0500

    feat: requiring user to login

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from flask import render_template, flash, redirect, url_for, request
-from flask_login import current_user, login_user, logout_user
+from flask_login import current_user, login_user, logout_user, login_required
 from werkzeug.urls import url_parse
 
 from app import app
@@ -9,6 +9,7 @@ from app.models import User
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
     user = {'username': 'Pamirkhan'}
     posts = [
