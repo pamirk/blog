59b8f0b7c8937beb935e410590664ec3dc39b791
Pamir khan <pmysteryk@gmail.com>
2023-07-11 19:24:38 +0500

    fix: typos in config.py file

--- config.py
+++ config.py
@@ -8,9 +8,9 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
                               'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    Mail_SERVER = os.environ.get('MAIL_SERVER')
-    Mail_PORT = int(os.environ.get('MAIL_PORT') or 25)
-    Mail_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
-    Mail_USERNAME = os.environ.get('MAIL_USERNAME')
-    Mail_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['pamirkhan11@gmail.com']
