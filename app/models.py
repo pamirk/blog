bc415bf56ae06272afc09dce35642c5809906251
Pamir khan <pmysteryk@gmail.com>
2023-07-12 23:10:57 +0500

    feat: adding and removing followers

--- app/models.py
+++ app/models.py
@@ -49,6 +49,18 @@ class User(UserMixin, db.Model):
         secondaryjoin=(followers.c.followed_id == id),  # right side entity
         backref=db.backref('followers', lazy='dynamic'), lazy='dynamic')  # right side entity
 
+    def follow(self, user):
+        if not self.is_following(user):
+            self.followed.append(user)
+
+    def unfollow(self, user):
+        if self.is_following(user):
+            self.followed.remove(user)
+
+    def is_following(self, user):
+        return self.followed.filter(
+            followers.c.followed_id == user.id).count() > 0
+
     def __repr__(self):
         return '<User {}>'.format(self.username)
 
