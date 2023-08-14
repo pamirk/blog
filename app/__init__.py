7519aa0cba8c33f9f058812c5062c8ab240c775c
Pamir khan <pmysteryk@gmail.com>
2023-08-14 13:12:15 +0500

    fix: refactor auth routes using blueprints

--- app/__init__.py
+++ app/__init__.py
@@ -37,6 +37,9 @@ def create_app(config_class=Config):
     from app.errors import bp as errors_bp
     app.register_blueprint(errors_bp)
 
+    from app.auth import bp as auth_bp
+    app.register_blueprint(auth_bp, url_prefix='/auth')
+
     if not app.debug and not app.testing:
         if app.config['MAIL_SERVER']:
             auth = None
