6ec7292c310da314bcb98dac461169133df49276
Pamir khan <pmysteryk@gmail.com>
2023-08-14 18:58:05 +0500

    feat: configured environment variables using .env file

--- config.py
+++ config.py
@@ -1,7 +1,8 @@
 import os
+from dotenv import load_dotenv
 
 basedir = os.path.abspath(os.path.dirname(__file__))
-
+load_dotenv(os.path.join(basedir, '.env'))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'MY-VERY-PRIVATE-AND-SECRETE-KEY-THAT-NO-ONE-CAN-GUESS'
