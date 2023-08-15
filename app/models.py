5236e9246fba65378e5ccd1c9b0ec7ecdc792e78
Pamir khan <pmysteryk@gmail.com>
2023-08-15 19:41:12 +0500

    make body attribute of Post modal searchable

--- app/models.py
+++ app/models.py
@@ -148,7 +148,8 @@ def load_user(userid):
     return User.query.get(int(userid))
 
 
-class Post(db.Model):
+class Post(SearchableMixin, db.Model):
+    __searchable__ = ['body']
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.String(140))
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
