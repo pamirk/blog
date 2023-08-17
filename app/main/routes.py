24edd4493f32292bfb58c0e23bcd0968b3fc1494
Pamir khan <pmysteryk@gmail.com>
2023-08-17 07:02:35 +0500

    use add_notification function in routes

--- app/main/routes.py
+++ app/main/routes.py
@@ -181,6 +181,7 @@ def send_message(recipient):
         msg = Message(author=current_user, recipient=user,
                       body=form.message.data)
         db.session.add(msg)
+        user.add_notification('unread_message_count', user.new_messages())
         db.session.commit()
         flash(_('Your message has been sent.'))
         return redirect(url_for('main.user', username=recipient))
@@ -192,6 +193,7 @@ def send_message(recipient):
 @login_required
 def messages():
     current_user.last_message_read_time = datetime.utcnow()
+    current_user.add_notification('unread_message_count', 0)
     db.session.commit()
     page = request.args.get('page', 1, type=int)
     messages = current_user.messages_received.order_by(
