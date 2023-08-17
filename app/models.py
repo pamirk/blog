c8f5d8b66ac076098a19d23e968b86170675bb6d
Pamir khan <pmysteryk@gmail.com>
2023-08-17 06:47:38 +0500

    add Notification model

--- app/models.py
+++ app/models.py
@@ -1,3 +1,4 @@
+import json
 from datetime import datetime
 from hashlib import md5
 from time import time
@@ -182,3 +183,14 @@ class Message(db.Model):
 
     def __repr__(self):
         return '<Message {}>'.format(self.body)
+
+
+class Notification(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128), index=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    timestamp = db.Column(db.Float, index=True, default=time)
+    payload_json = db.Column(db.Text)
+
+    def get_data(self):
+        return json.loads(str(self.payload_json))
